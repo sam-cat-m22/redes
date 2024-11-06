a1f5fb0dcf1bba022748614c06b561c348a712fb
Cesar H <ctorresg2100@alumno.ipn.mx>
2024-11-06 12:43:05 -0600

    esto jala

--- src/practica_2/Practica_2.java
+++ src/practica_2/Practica_2.java
@@ -17,6 +17,7 @@ public class Practica_2 {
      */
     public static void main(String[] args) {
         System.out.println("Esto es una prueba");
+        System.out.println("hola");
     }
     
 }
