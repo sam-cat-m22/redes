1b609ed2a38d0f355bae31375e1a328995b37db0
Cesar H <ctorresg2100@alumno.ipn.mx>
2024-11-06 12:41:36 -0600

    esto jala

--- src/practica_2/Practica_2.java
+++ src/practica_2/Practica_2.java
@@ -16,7 +16,7 @@ public class Practica_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Esto es una prueba");
     }
     
 }
